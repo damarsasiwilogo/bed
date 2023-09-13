da6214f7088a8ef73a62ca2626e6780adb2147e1
damarsasiwilogo <damarsasiwilogo@gmail.com>
2023-09-13 12:43:37 +0700

    add session03

---
+++ session03-exrcs/db.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+
+export function getUserData() {
+  const db = fs.readFileSync("./db.json");
+  const json = String(db);
+  const obj = JSON.parse(json);
+  return obj.users;
+}
+
+export function createNewUsers(newUser) {
+  const db = fs.readFileSync("./db.json");
+  const json = String(db);
+  const obj = JSON.parse(json);
+  obj.users.push(newUser);
+  fs.writeFileSync("./db.json", JSON.stringify(obj));
+}
